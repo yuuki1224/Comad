18a393f0d03fd41a77a1cdcf34acd142afb5717d
YukiAsano <yuuki.1224.softtennis@gmail.com>
2014-07-20 18:28:05 +0900

    Add CMDProductionDescriptionView

---
+++ Comad/Resources/StoryBoards/CMDProductionDescriptionView.m
@@ -0,0 +1,31 @@
+//
+//  CMDProductionDescriptionView.m
+//  Comad
+//
+//  Created by AsanoYuki on 2014/07/20.
+//  Copyright (c) 2014年 AsanoYuki. All rights reserved.
+//
+
+#import "CMDProductionDescriptionView.h"
+
+@implementation CMDProductionDescriptionView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
