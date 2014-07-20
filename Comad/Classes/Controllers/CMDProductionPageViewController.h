2e66d084f4ad0c0cf360818f63d384c4b727d22c
YukiAsano <yuuki.1224.softtennis@gmail.com>
2014-07-20 18:27:46 +0900

    Add CMDProductionPageViewController

---
+++ Comad/Classes/Controllers/CMDProductionPageViewController.h
@@ -0,0 +1,15 @@
+//
+//  CMDProductionPageViewController.h
+//  Comad
+//
+//  Created by AsanoYuki on 2014/07/20.
+//  Copyright (c) 2014年 AsanoYuki. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CMDProductionPageViewController : UIViewController
+
+@property (weak, nonatomic) IBOutlet UIScrollView *contentScrollView;
+
+@end
