6d07a0f2a760eec3341e2cb3f0b3420f94eaf256
YukiAsano <yuuki.1224.softtennis@gmail.com>
2014-07-20 18:38:51 +0900

    Add CMDRelatedProductionView

---
+++ Comad/Classes/Views/CMDRelatedProductionView.m
@@ -0,0 +1,31 @@
+//
+//  CMDRelatedProductionView.m
+//  Comad
+//
+//  Created by AsanoYuki on 2014/07/20.
+//  Copyright (c) 2014年 AsanoYuki. All rights reserved.
+//
+
+#import "CMDRelatedProductionView.h"
+
+@implementation CMDRelatedProductionView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
